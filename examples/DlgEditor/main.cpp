de1ab0fe26bb130ae0e48f694915a8b4957bae21
Asil Zogby <asilz@stud.ntnu.no>
2024-06-05 16:55:01 +0200

    Main allows choosing which files to open using input arguments

--- examples/DlgEditor/main.cpp
+++ examples/DlgEditor/main.cpp
@@ -1,11 +1,38 @@
-#include "include/widgets.hpp"
+#include <gui.hpp>
+extern "C"
+{
+#include <stream.h>
+}
 
 int Main(int argc, char **argv)
 {
-    DlgApplication exampe("Widgets", argc, argv);
+    int ret;
+    if (argc < 2)
+    {
+        printf("Too few arguments\n");
+        return -1;
+    }
+    FILE *inputStream = cfopen(argv[1], "rb");
+    if (inputStream == NULL)
+    {
+        printf("invalid input path\n");
+        return -2;
+    }
+    DlgApplication exampe("Blueprints", inputStream);
 
     if (exampe.Create())
-        return exampe.Run();
+        ret = exampe.Run();
+
+    if (argc == 3)
+    {
+        FILE *outputStream = fopen(argv[2], "wb");
+        printf("argc %d\n", argc);
+        writeMetaStreamHeader(outputStream, &(exampe.header));
+        writeTree(outputStream, &(exampe.dlg));
+        fclose(outputStream);
+    }
+
+    return ret;
 
     return 0;
 }
