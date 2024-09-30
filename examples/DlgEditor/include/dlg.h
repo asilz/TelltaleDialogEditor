922719ba5306a9f362ac5a3ee5dadf6d535411e2
Asil Zogby <asilz@stud.ntnu.no>
2024-09-30 22:25:01 +0200

    Add missing read function

--- examples/DlgEditor/include/dlg.h
+++ examples/DlgEditor/include/dlg.h
@@ -37,6 +37,7 @@ int DlgChildRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgChildSetRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgChildSetChoiceRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgConditionSetRead(FILE *stream, struct TreeNode *node, uint32_t flags);
+int DlgChildSetConditionalCaseRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgChainHeadRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgConditionRead(FILE *stream, struct TreeNode *node, uint32_t flags);
 int DlgChildSetElementRead(FILE *stream, struct TreeNode *node, uint32_t flags);
